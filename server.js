28fd741393f7893e7e69a946130f705f41aef514
jKongEdu <89545639+jKongEdu@users.noreply.github.com>
2022-02-19 10:58:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,18 +11,21 @@ app.use(bodyParser.json());
 const cors = require("cors");
 app.use(cors());
 
-// Create a local server to test, port 8080
-const port = 8080;
+// Initialize the main project folder
+app.use(express.static('website'));
 
+// Create a local server to test
+const port = 8000;
 // Create a server using the .listen() method
 const server = app.listen(port, listening);
 
 function listening(){
-    console.log(`Server is running on localhost port: ${port}.`)
+    console.log(`The server is running.`)
+    console.log(`Localhost: ${port}.`)
 };
 
 //  Set data endpoint for routes
-const projectData = [];
+const projectData = {};
 
 /////////////////////////////////////////////
 /////////////////////////////////////////////
@@ -30,10 +33,9 @@ const projectData = [];
 /////////////////////////////////////////////
 /////////////////////////////////////////////
 
-app.get("/", returnProjectData);
+app.get("/all", returnProjectData);
 
 function returnProjectData(req, res){
-    console.log("Sending a response back.")
     res.send(projectData);
 }
 
@@ -43,18 +45,21 @@ function returnProjectData(req, res){
 /////////////////////////////////////////////
 /////////////////////////////////////////////
 
-let temp, date, userResp;
 
-app.post("/projectData", sendProjectData);
+app.post("/addData", addData);
 
-function sendProjectData(req, res){
-    let newEntry = {
-        temperature: req.body.temp,
-        date: req.body.date,
-        userResp: req.body.userRes
-    }
+function addData(req, res){
+    // Create data var to hold req.body
+    let data = req.body;
+    console.log(`Server side code: ${data}`);
 
-    projectData.push(newEntry);
-    console.log(newEntry);
+    //  Post new properties/types for JS object endpoint "projectData"
+    projectData['temp'] = data.temp;
+    projectData['userFeeling'] = data.userFeeling;
+    projectData['date'] = data.date;
+
+    //  Send response back to the the endpoint
+    res.send(projectData);
+    console.log(`projecctData = ${projectData}`);
 };
 
