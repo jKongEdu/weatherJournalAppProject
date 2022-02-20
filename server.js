f0b2e4db76ebecba32315dd622e83c92d1601b89
jKongEdu <89545639+jKongEdu@users.noreply.github.com>
2022-02-20 18:03:06 -0500

    Update server.js main
    This is the final code for projecct submission.

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 // Server file - Remember hierarchy, server file typically exists on the same level as the website folder
 
-//  Initialize express and middle ware
-const express = require("express");
-const app = express();
-
+//  GLOBAL vars: Initialize express,  middle ware, and server
+const express    = require("express");
+const app        = express();
 const bodyParser = require("body-parser");
+const cors       = require("cors");
+const port       = 8000;
+const server     = app.listen(port, listening);
+
 app.use(bodyParser.urlencoded( {extended: false} ));
 app.use(bodyParser.json());
-
-const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-// Create a local server to test
-const port = 8000;
-// Create a server using the .listen() method
-const server = app.listen(port, listening);
 
 function listening(){
-    console.log(`The server is running.`)
-    console.log(`Localhost: ${port}.`)
+    console.log(`The server is running.`);
+    console.log(`Localhost: ${port}.`);
 };
 
 //  Set data endpoint for routes
@@ -36,6 +33,7 @@ const projectData = {};
 app.get("/all", returnProjectData);
 
 function returnProjectData(req, res){
+    console.log(`sending prooject data:`);
     res.send(projectData);
 }
 
@@ -51,15 +49,15 @@ app.post("/addData", addData);
 function addData(req, res){
     // Create data var to hold req.body
     let data = req.body;
-    console.log(`Server side code: ${data}`);
 
     //  Post new properties/types for JS object endpoint "projectData"
     projectData['temp'] = data.temp;
     projectData['userFeeling'] = data.userFeeling;
     projectData['date'] = data.date;
+    projectData['time'] = data.time;
 
     //  Send response back to the the endpoint
     res.send(projectData);
-    console.log(`projecctData = ${projectData}`);
+    console.log(data);
 };
 
